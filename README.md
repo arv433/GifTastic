93457520f733cab6068957b86efb32c06ff54539
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-08-07 14:13:12 -0400

    Added README file, added download functionality which needs to be worked on further, minor adjustments

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # GifTastic
+
+This application communicates with two APIs, IGDB and Giphy. Upon searching a a title of any video game, the the IGDB API is sent a request for a matching video game title and returns a button of the same or similar name as the term searched for.
+
+![Search Screenshot](assets/images/GifTasticMain.png)
+
+The application promptly searches the Giphy API for ten GIFs related to that videogame title and displays it on the page. This search process is optimized to return image results as soon as the term is searched for, or if one of the default buttons are clicked on.
+
+Click the star to favorite or unfavorite a GIF, which is then stored in the favorites tab (even if the page reloads or exits), or click the download button to download a copy of the file to your computer.
+
+![Favorites Screenshot](assets/images/GifTasticFav.png)
+
+This application is also mobile-friendly
+
+Created by Arvin Zojaji
+
+Have fun!
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,24 @@
+/* The names of the initially created buttons are stored in an object as keys with null values.
+They are assigned an array of 10 jQuery objects which include the images pulled from the Giphy API.
+They are initially assigned the null value so that their corresponding API call is made when its button
+is clicked on, instead of making an API call for each of the 10 games when the page first loads */
 var gameButtons = { "Metal Gear Solid": null, "Super Mario 64": null, "God of War": null, "Grand Theft Auto": null, "Detroit: Become Human": null, "Heavy Rain": null, "Superhot": null, "Fallout": null, "Kirby and the Rainbow Curse": null, "Super Smash Bros.": null };
 
+/* favoritesArray is created from the localStorage and is an array of 4-element sub-arrays:
+array[0] is the link to the still version of the gif
+array[1] is the link to the animated version of the gif
+array[2] is the alt name of the gif
+array[3] is the MPAA-style rating of the gif */
 var favoritesArray = JSON.parse(localStorage.getItem("favorites"));
 
+// clear local storage if the favoritesArray does not exist or empty
 if (!Array.isArray(favoritesArray) || !favoritesArray.length) {
     localStorage.clear();
 };
 
+$("#loadingImage").hide();
+
+// this function sythesizes the gifs in the favorites screen from the array based on the local storage
 function populateFavs() {
     $("#favoriteGifsDisplay").empty();
     if (!(favoritesArray === null)) {
@@ -18,19 +31,22 @@ function populateFavs() {
             favGifElement.attr("data-animate", favoritesArray[m][1]);
             favGifElement.attr("src", favoritesArray[m][0]);
             favGifElement.attr("data-state", "still");
+            favGifElement.attr("rating", favoritesArray[m][3]);
             favGifElement.addClass("gifImage");
             var favicon = $("<span>");
             favicon.addClass("glyphicon glyphicon-star star");
+            var rating = $("<span>").text("Rated: " + favGifElement.attr("rating"));
+            rating.addClass("rating");
             favGifDiv.append(favGifElement);
             favGifDiv.append(favicon);
+            favGifDiv.append(rating);
             $("#favoriteGifsDisplay").prepend(favGifDiv);
         };
         addClickHandlersToImages();
     };
 };
 
-$("#loadingImage").hide();
-
+// this function renders the buttons from the gameButtons object
 function renderButtons() {
     $("#buttonsContainer").empty();
     for (var i = 0; i <= Object.keys(gameButtons).length - 1; i++) {
@@ -43,6 +59,7 @@ function renderButtons() {
     };
 };
 
+// this function adds click handlers to each button once they are rendered
 function addClickHandlersToButtons() {
     $(".videogameButton").unbind().click(function () {
         var title = $(this).attr("data-name");
@@ -50,19 +67,28 @@ function addClickHandlersToButtons() {
     });
 };
 
+/* This function checks to see if the button is a default, having an initial null value or not.
+If it is not a default, it appends the associated images to the display, shown in the else statement.
+If it is, as shown in the if statement the function calls addGifArray to populate the gameButton object
+then calls back to itself to complete the else statement */
 function createImages(titleClickedOn) {
     if (gameButtons[titleClickedOn] === null) {
         $("#loadingImage").show();
         addGifArray(titleClickedOn, createImages);
     } else {
-        $("#gifDisplay").empty();
+
         for (var k = 0; k <= gameButtons[titleClickedOn].length - 1; k++) {
-            $("#gifDisplay").append(gameButtons[titleClickedOn][k]);
+            $("#gifDisplay").prepend(gameButtons[titleClickedOn][k]);
         };
         addClickHandlersToImages();
     };
 }
 
+/* This function includes an optional callback that executes once the AJAX call is complete in case the
+button it is populating images for has a null value in the gameButton object. This is done to set a smooth
+user experience; if the user searches a game, the links pulled from the Giphy API become already available
+in one click. If the button already exists, the button makes the AJAX call when clicked on instead of loading
+all of the images when the page first loads. */
 var addGifArray = function (title, callback = null) {
     if (favoritesArray === null || favoritesArray.length == 0) {
         localStorage.clear();
@@ -76,7 +102,7 @@ var addGifArray = function (title, callback = null) {
         console.log(GiphyResponse);
         for (var j = 0; j <= GiphyResponse.data.length - 1; j++) {
             // creating container
-            var gifDiv = $("<div>")
+            var gifDiv = $("<div>");
             gifDiv.addClass("gifDiv");
             // creating gif
             var gifElement = $("<img>");
@@ -87,13 +113,14 @@ var addGifArray = function (title, callback = null) {
             gifElement.attr("data-state", "still");
             gifElement.attr("rating", GiphyResponse.data[j].rating.toUpperCase());
             gifElement.addClass("gifImage");
+            // adding rating
+            var rating = $("<span>").text("Rated: " + gifElement.attr("rating"));
+            rating.addClass("rating");
             // adding favicon
             var favicon = $("<span>");
             favicon.addClass("glyphicon star");
-            var rating = $("<span>").text("Rated: " + gifElement.attr("rating"));
-            rating.addClass("rating");
+            // this if statement checks whether if the gif is a favorite or not, giving it the right star when pulled from Giphy
             if (!Array.isArray(favoritesArray) || !favoritesArray.length) {
-                console.log("not an array")
                 favicon.addClass("glyphicon-star-empty");
             } else {
                 for (var l = 0; l <= favoritesArray.length - 1; l++) {
@@ -104,10 +131,19 @@ var addGifArray = function (title, callback = null) {
                     };
                 };
             };
+            // adding download button
+            var downloadLink = $("<a>");
+            downloadLink.addClass("downloadBtnContainer")
+            downloadLink.attr("href", GiphyResponse.data[j].images.looping.mp4);
+            downloadLink.attr("download", "")
+            var downloadImg = $("<span>");
+            downloadImg.addClass("glyphicon glyphicon-download-alt downloadBtn");
+            downloadLink.append(downloadImg);
             // adding components to container Div
             gifDiv.append(gifElement);
             gifDiv.append(favicon);
             gifDiv.append(rating);
+            gifDiv.append(downloadLink);
             // adding div to array
             imgArray.push(gifDiv);
             gameButtons[title] = imgArray;
@@ -120,6 +156,7 @@ var addGifArray = function (title, callback = null) {
     });
 };
 
+// this function adds the click handlers to play and stop the gifs, as well as the star button to favorite gifs
 function addClickHandlersToImages() {
     $(".gifImage").on("click", function () {
         if ($(this).attr("data-state") == "still") {
@@ -138,7 +175,7 @@ function addClickHandlersToImages() {
         if ($(this).hasClass("glyphicon-star-empty")) {
             $(this).removeClass("glyphicon-star-empty");
             $(this).addClass("glyphicon-star");
-            favoritesArray.push([$(this).prev().attr("data-still"), $(this).prev().attr("data-animate"), $(this).prev().attr("alt")]);
+            favoritesArray.push([$(this).prev().attr("data-still"), $(this).prev().attr("data-animate"), $(this).prev().attr("alt"), $(this).prev().attr("rating")]);
             localStorage.setItem("favorites", JSON.stringify(favoritesArray));
 
             // if aleady favorited:
@@ -151,7 +188,7 @@ function addClickHandlersToImages() {
                     favoritesArray.splice(favoritesArray.indexOf(favoritesArray[n]), 1);
                     localStorage.setItem("favorites", JSON.stringify(favoritesArray));
                 };
-            }; // move under here
+            };
             for (var o = 0; o <= Object.keys(gameButtons).length - 1; o++) {
                 if (!(gameButtons[Object.keys(gameButtons)[o]] === null)) {
                     for (var p = 0; p <= gameButtons[Object.keys(gameButtons)[o]].length - 1; p++) {
@@ -163,12 +200,14 @@ function addClickHandlersToImages() {
                     };
                 };
             };
+            addClickHandlersToImages();
         };
         populateFavs();
-        addClickHandlersToImages();
     });
 };
 
+/* This function searches for a videogame title on IGDB then calls addGifArray to immediately find the associated images in one click.
+Clicking the resulting button of the title searched for immediately pulls the images pull from Giphy instead of an additional AJAX call */
 $("#videogameSubmit").on("click", function (event) {
     event.preventDefault();
     var IGDBGameTitle = $("#videogameInput").val().trim();
@@ -200,5 +239,6 @@ $("#videogameSubmit").on("click", function (event) {
     });
 });
 
+// initialization functions
 renderButtons();
 populateFavs();
